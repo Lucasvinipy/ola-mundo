e1c846e31f051db79e5571d5a25010b2eb8cf6dd
lucas vinicius reis de oliveira <lucasvini6@hotmail.com>
2024-06-28 18:02:14 -0300

    primeiro codigo
    teste de um codigo de area

---
+++ area.py
@@ -0,0 +1,6 @@
+print('calculadora de area e potencia de iluminaçao')
+altura=float(input('digite altura do comodo:'))
+largura=float(input('digite largura do comodo'))
+area=altura*largura
+luz=area*18
+print('a area é igual a{} e a luz é igual a {}'.format(area,luz))
